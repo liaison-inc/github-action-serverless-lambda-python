16c91c8c64b58756b12887118b1b6965f410b90b
Macario Lara <ml27299@gmail.com>
2021-07-22 00:47:58 -0500

    update

--- entry.sh
+++ entry.sh
@@ -20,6 +20,12 @@ if [[ -z $ARTIFACT_REPOSITORY ]]; then
   exit 1
 fi
 
-CODEARTIFACT_TOKEN="$(aws codeartifact get-authorization-token --domain ${ARTIFACT_DOMAIN} --domain-owner ${ARTIFACT_DOMAIN_OWNER} --query authorizationToken --output text)"
-export PIPENV_PYPI_MIRROR=https://aws:${CODEARTIFACT_TOKEN}@${ARTIFACT_DOMAIN}-${ARTIFACT_DOMAIN_OWNER}.d.codeartifact.${ARTIFACT_REGION}.amazonaws.com/pypi/${ARTIFACT_REPOSITORY}/simple/
+if [[ -n $ARTIFACT_REGION ]]; then
+  CODEARTIFACT_TOKEN="$(aws codeartifact get-authorization-token --region $ARTIFACT_REGION --domain ${ARTIFACT_DOMAIN} --domain-owner ${ARTIFACT_DOMAIN_OWNER} --query authorizationToken --output text)"
+  export PIPENV_PYPI_MIRROR=https://aws:${CODEARTIFACT_TOKEN}@${ARTIFACT_DOMAIN}-${ARTIFACT_DOMAIN_OWNER}.d.codeartifact.${ARTIFACT_REGION}.amazonaws.com/pypi/${ARTIFACT_REPOSITORY}/simple/
+else
+  CODEARTIFACT_TOKEN="$(aws codeartifact get-authorization-token --domain ${ARTIFACT_DOMAIN} --domain-owner ${ARTIFACT_DOMAIN_OWNER} --query authorizationToken --output text)"
+  export PIPENV_PYPI_MIRROR=https://aws:${CODEARTIFACT_TOKEN}@${ARTIFACT_DOMAIN}-${ARTIFACT_DOMAIN_OWNER}.d.codeartifact.${ARTIFACT_REGION}.amazonaws.com/pypi/${ARTIFACT_REPOSITORY}/simple/
+fi
+
 eval sls "$@"
